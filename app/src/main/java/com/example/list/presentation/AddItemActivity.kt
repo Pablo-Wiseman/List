7b4468289771499ce12733987240d47f9a9b3dad
Pablo Wiseman <accessdevelopment@yandex.ru>
2023-12-28 20:57:39 +0500

    AddItemActivity created

---
+++ app/src/main/java/com/example/list/presentation/AddItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.list.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.list.R
+
+class AddItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+}
